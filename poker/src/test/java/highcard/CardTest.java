ea7fdd4e9d9e8edaa260b1c701078dce940beea5
homework <52529600+SKYE-633@users.noreply.github.com>
2019-08-11 15:33:10 +0800

    Test3 should_return_draw_when_give_two_cardList

--- poker/src/test/java/highcard/CardTest.java
+++ poker/src/test/java/highcard/CardTest.java
@@ -29,5 +29,15 @@ public class CardTest {
         //then
         Assert.assertEquals("winner2",result);
     }
-
+    @Test
+    public void should_return_draw_when_give_two_cardList() {
+        //given
+        Poker poker = new Poker();
+        List<Card> cards1 = Arrays.asList(new Card("2C"), new Card("3C"), new Card("QH"), new Card("7H"), new Card("8C"));
+        List<Card> cards2 = Arrays.asList(new Card("2D"), new Card("3D"), new Card("QS"), new Card("7C"), new Card("8H"));
+        //when
+        String result = poker.compare(cards1, cards2);
+        //then
+        Assert.assertEquals("draw", result);
+    }
 }
